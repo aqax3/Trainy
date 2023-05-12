5f5fc6e073eebf74b6bd96bb285dbc50927a866e
Luka <luka.moleh@student.um.si>
2023-05-13 00:59:05 +0200

    frontend backend connection test

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,10 +4,15 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import User from "./User";
+
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI as string);
+mongoose
+  .connect(process.env.MONGO_URI as string)
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.error("MongoDB connection error: ", err));
 
 // Middleware
 app.use(cors());
@@ -18,6 +23,24 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+
+app.post("/add-user", async (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).send("Missing name");
+  }
+
+  const user = new User({ name });
+  
+  try {
+    await user.save();
+    res.send(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+})
 
 // Start the server
 const PORT = process.env.PORT || 5000;
--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,10 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+
+import axios from "axios";
 
 export default function App() {
+  const [name, setName] = useState("");
+
+  const addUser = () => {
+    const data = {
+      name: name,
+    };
+
+    axios
+      .post("http://192.168.1.110:5000/add-user", data)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    setName("");
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+        placeholder="Enter name"
+      />
+      <Button title="Add User" onPress={addUser} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +42,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginTop: 20,
+    marginBottom: 20,
+    width: "80%",
+    padding: 10,
   },
 });
