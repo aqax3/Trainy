ff7cb2923728430a9b00482e6d67b900dd21ed2c
aqax3 <47643485+aqax3@users.noreply.github.com>
2023-07-13 15:52:23 +0200

    #

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,6 +1,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 const Stack = createNativeStackNavigator();
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
@@ -35,10 +36,13 @@ import * as eva from "@eva-design/eva";
 >>>>>>> parent of 9998372 (Design)
 =======
 >>>>>>> parent of 9998372 (Design)
+=======
+>>>>>>> parent of 9998372 (Design)
 import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 import React, { useState } from "react";
 import NavigationRegLog from "./components/NavigationRegLog/NavigationRegLog";
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 >>>>>>> parent of 9998372 (Design)
@@ -258,6 +262,25 @@ const App = () => {
 };
 export default App;
 =======
+  return (
+    <View style={styles.container}>
+      <NavigationRegLog />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  }
+});
+>>>>>>> parent of 9998372 (Design)
+=======
+
+export default function App() {
+
   return (
     <View style={styles.container}>
       <NavigationRegLog />
