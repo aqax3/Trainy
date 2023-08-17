8a6a253840c8a36bf2f8b7b7f38cc0c9a752cc56
aqax3 <47643485+aqax3@users.noreply.github.com>
2023-08-17 15:15:40 +0200

    myworkouts design

--- frontend/components/Workouts/myWorkout.tsx
+++ frontend/components/Workouts/myWorkout.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback } from "react";
-import { View, Button, Text, TouchableOpacity } from "react-native";
+import { View, Button, Text, TouchableOpacity, StyleSheet } from "react-native";
 import axios from "axios";
 import { useNavigation, useFocusEffect } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -14,17 +14,15 @@ const MyWorkoutScreen = () => {
       const fetchWorkouts = async () => {
         try {
           const userToken = await AsyncStorage.getItem("userToken");
-          const response = await axios.get(
-            "http://localhost:5001/workouts",
-            {
-              headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${userToken}`,
-              },
-            }
-          );
+          const response = await axios.get("http://localhost:5001/workouts", {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${userToken}`,
+            },
+          });
 
           setWorkouts(response.data);
+          console.log(response.data);
         } catch (error) {
           console.error(error);
         }
@@ -44,6 +42,7 @@ const MyWorkoutScreen = () => {
           );
 
           setAdminWorkouts(response.data);
+          console.log(response.data);
         } catch (error) {
           console.error(error);
         }
@@ -52,17 +51,18 @@ const MyWorkoutScreen = () => {
       fetchWorkouts();
       fetchAdminWorkouts();
 
-      // Optional: Return a cleanup function if needed
-      return () => {
-        // Cleanup tasks if any
-      };
+      return () => {};
     }, [])
   );
 
+  
+
   return (
-    <View>
-      <View>
-        <Text>MY WORKOUTS</Text>
+    <View style={{ flex: 1, backgroundColor: "#1a2d3d", padding: 10 }}>
+      <View style={{ marginBottom: 20 }}>
+        <Text style={[styles.text, { fontSize: 20, fontWeight: "bold", alignSelf: 'center' }]}>
+          MY WORKOUTS
+        </Text>
         {workouts.map((workout, index) => (
           <View
             key={index}
@@ -71,24 +71,30 @@ const MyWorkoutScreen = () => {
               justifyContent: "space-between",
               alignItems: "center",
               marginBottom: 10,
+              padding: 5,
+              backgroundColor: "#2e4a62",
+              borderRadius: 5,
             }}
           >
             <TouchableOpacity
+              style={styles.buttonWrapper}
               onPress={() =>
                 navigation.navigate("WorkoutDetail", { workoutId: workout._id })
               }
             >
-              <Text>{workout.name}</Text>
+              <Text style={styles.text}>{workout.name}</Text>
             </TouchableOpacity>
-            <Button
-              title="Edit"
-              onPress={() => {
-                navigation.navigate("EditWorkout", { workoutId: workout._id });
-              }}
-            />
-            <Button
-              title="Add to Calendar"
-              onPress={() => {
+            <TouchableOpacity
+              style={styles.buttonWrapper}
+              onPress={() =>
+                navigation.navigate("EditWorkout", { workoutId: workout._id })
+              }
+            >
+              <Text style={styles.text}>Edit</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.buttonWrapper}
+              onPress={() =>
                 navigation.navigate("App", {
                   screen: "HomeDrawer",
                   params: {
@@ -101,18 +107,29 @@ const MyWorkoutScreen = () => {
                       },
                     },
                   },
-                });
-              }}
-            />
+                })
+              }
+            >
+              <Text style={styles.text}>Add to Calendar</Text>
+            </TouchableOpacity>
           </View>
         ))}
-        <Button
-          title="Create New Workout"
-          onPress={() => navigation.navigate("CreateWorkoutScreen")}
-        />
+        <View
+          style={{ justifyContent: "center", alignItems: "center" }}
+        >
+          <TouchableOpacity
+            onPress={() => navigation.navigate("CreateWorkoutScreen")}
+          >
+            <View style={styles.textWrapper}>
+              <Text style={styles.text}>Create New Workout</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </View>
       <View>
-        <Text>COMMUNITY WORKOUTS</Text>
+        <Text style={{ color: "#e5f4e3", fontSize: 20, fontWeight: "bold", alignSelf: 'center' }}>
+          COMMUNITY WORKOUTS
+        </Text>
         {adminWorkouts.map((workout, index) => (
           <View
             key={index}
@@ -121,17 +138,21 @@ const MyWorkoutScreen = () => {
               justifyContent: "space-between",
               alignItems: "center",
               marginBottom: 10,
+              padding: 5,
+              backgroundColor: "#2e4a62",
+              borderRadius: 5,
             }}
           >
             <TouchableOpacity
+              style={styles.buttonWrapper}
               onPress={() =>
                 navigation.navigate("WorkoutDetail", { workoutId: workout._id })
               }
             >
-              <Text>{workout.name}</Text>
+              <Text style={{ color: "#e5f4e3" }}>{workout.name}</Text>
             </TouchableOpacity>
-            <Button
-              title="Add to Calendar"
+            <TouchableOpacity
+              style={styles.buttonWrapper}
               onPress={() => {
                 navigation.navigate("App", {
                   screen: "HomeDrawer",
@@ -147,12 +168,41 @@ const MyWorkoutScreen = () => {
                   },
                 });
               }}
-            />
+            >
+              <Text style={styles.text}>Add to Calendar</Text>
+            </TouchableOpacity>
           </View>
         ))}
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  buttonWrapper: {
+    backgroundColor: "#2e4a62",
+    borderRadius: 20,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 5,
+    marginHorizontal: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderWidth: 1,
+    borderColor: "#e5f4e3",
+  },
+  text: {
+    color: "#e5f4e3",
+  },
+  textWrapper: {
+    borderColor: "#e5f4e3",
+    borderWidth: 1,
+    borderRadius: 20,
+    paddingHorizontal: 10,
+    paddingVertical: 5, // Adjust this value to manage the spacing around the text
+    alignSelf: "flex-start", // To make the view wrap only around the text content
+  },
+});
 
 export default MyWorkoutScreen;
