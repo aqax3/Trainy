f50882f31d849b5bb3a35ea6943061d15cb3e339
MolehLuka <luka.moleh@student.um.si>
2023-08-17 02:21:05 +0200

    statistics minor fix

--- frontend/components/Statistics/Statstics.tsx
+++ frontend/components/Statistics/Statstics.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { View, Text, ScrollView, StyleSheet } from "react-native";
 import { LineChart, BarChart, XAxis } from "react-native-svg-charts";
 import { IWorkout } from '../../../backend/src/schemas/Workout';
+import { useFocusEffect } from '@react-navigation/native';
 
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -35,28 +36,33 @@ const getAxiosConfig = async () => {
 function CompletedWorkouts() {
   const [completedWorkouts, setCompletedWorkouts] = useState(0);
 
-  useEffect(() => {
-    const fetchCompletedWorkouts = async () => {
-      try {
-        const config = await getAxiosConfig();
-        const response = await axios.get(
-          "http://192.168.1.106:5001/completedWorkouts",
-          config
-        );
-        console.log("Completed Workouts Response:", response.data);
-        setCompletedWorkouts(response.data.completedWorkouts);
-      } catch (error) {
-        console.error("Axios Error:", error);
-      }
-    };
+  useFocusEffect(
+    React.useCallback(() => {
+      const fetchCompletedWorkouts = async () => {
+        try {
+          const config = await getAxiosConfig();
+          const response = await axios.get(
+            "http://192.168.1.106:5001/completedWorkouts",
+            config
+          );
+          console.log("Completed Workouts Response:", response.data);
+          setCompletedWorkouts(response.data.completedWorkouts);
+        } catch (error) {
+          console.error("Axios Error:", error);
+        }
+      };
 
-    fetchCompletedWorkouts();
-  }, []);
+      fetchCompletedWorkouts();
+
+      // Cleanup function if needed
+      return () => {};
+    }, [])
+  );
 
   return (
     <View style={styles.card}>
       <Text style={styles.title}>Completed Workouts</Text>
-      <Text style={styles.value}>{completedWorkouts}</Text>
+      <Text style={styles.text}>{completedWorkouts}</Text>
     </View>
   );
 }
@@ -113,28 +119,33 @@ function ExerciseTypeStats() {
 function AverageWorkoutDuration() {
   const [averageDuration, setAverageDuration] = useState(0);
 
-  useEffect(() => {
-    const fetchAverageWorkoutDuration = async () => {
-      try {
-        const config = await getAxiosConfig();
-        const response = await axios.get(
-          "http://192.168.1.106:5001/averageWorkoutDuration",
-          config
-        );
-        setAverageDuration(response.data.averageDuration);
-        console.log(response.data);
-      } catch (error) {
-        console.error("Axios Error:", error);
-      }
-    };
+  useFocusEffect(
+    React.useCallback(() => {
+      const fetchAverageWorkoutDuration = async () => {
+        try {
+          const config = await getAxiosConfig();
+          const response = await axios.get(
+            "http://192.168.1.106:5001/averageWorkoutDuration",
+            config
+          );
+          setAverageDuration(response.data.averageDuration);
+          console.log(response.data);
+        } catch (error) {
+          console.error("Axios Error:", error);
+        }
+      };
 
-    fetchAverageWorkoutDuration();
-  }, []);
+      fetchAverageWorkoutDuration();
+
+      // Cleanup function if needed
+      return () => {};
+    }, [])
+  );
 
   return (
     <View style={styles.card}>
       <Text style={styles.title}>Average Workout Duration</Text>
-      <Text style={styles.value}>{averageDuration} mins</Text>
+      <Text style={styles.text}>{averageDuration} mins</Text>
     </View>
   );
 }
@@ -143,30 +154,35 @@ function LongestShortestWorkout() {
   const [longestWorkout, setLongestWorkout] = useState(0);
   const [shortestWorkout, setShortestWorkout] = useState(0);
 
-  useEffect(() => {
-    const fetchLongestAndShortestWorkout = async () => {
-      try {
-        const config = await getAxiosConfig();
-        const response = await axios.get(
-          "http://192.168.1.106:5001/workoutLengthStats",
-          config
-        );
-        setLongestWorkout(response.data.longest);
-        setShortestWorkout(response.data.shortest);
-      } catch (error) {
-        console.error("Axios Error:", error);
-      }
-    };
+  useFocusEffect(
+    React.useCallback(() => {
+      const fetchLongestAndShortestWorkout = async () => {
+        try {
+          const config = await getAxiosConfig();
+          const response = await axios.get(
+            "http://192.168.1.106:5001/workoutLengthStats",
+            config
+          );
+          setLongestWorkout(response.data.longest);
+          setShortestWorkout(response.data.shortest);
+        } catch (error) {
+          console.error("Axios Error:", error);
+        }
+      };
 
-    fetchLongestAndShortestWorkout();
-  }, []);
+      fetchLongestAndShortestWorkout();
+
+      // Cleanup function if needed
+      return () => {};
+    }, [])
+  );
 
   return (
     <View style={styles.card}>
       <Text style={styles.title}>Longest Workout</Text>
-      <Text style={styles.value}>{longestWorkout} mins</Text>
+      <Text style={styles.text}>{longestWorkout} mins</Text>
       <Text style={styles.title}>Shortest Workout</Text>
-      <Text style={styles.value}>{shortestWorkout} mins</Text>
+      <Text style={styles.text}>{shortestWorkout} mins</Text>
     </View>
   );
 }
@@ -174,28 +190,33 @@ function LongestShortestWorkout() {
 function MostCommonExercise() {
   const [mostCommonExercise, setMostCommonExercise] = useState("");
 
-  useEffect(() => {
-    const fetchMostCommonExercise = async () => {
-      try {
-        const config = await getAxiosConfig();
-        const response = await axios.get(
-          "http://192.168.1.106:5001/mostCommonExercise",
-          config
-        );
-        console.log(response.data);
-        setMostCommonExercise(response.data.mostCommonExercise);
-      } catch (error) {
-        console.error("Axios Error:", error);
-      }
-    };
+  useFocusEffect(
+    React.useCallback(() => {
+      const fetchMostCommonExercise = async () => {
+        try {
+          const config = await getAxiosConfig();
+          const response = await axios.get(
+            "http://192.168.1.106:5001/mostCommonExercise",
+            config
+          );
+          console.log(response.data);
+          setMostCommonExercise(response.data.mostCommonExercise);
+        } catch (error) {
+          console.error("Axios Error:", error);
+        }
+      };
 
-    fetchMostCommonExercise();
-  }, []);
+      fetchMostCommonExercise();
+
+      // Cleanup function if needed
+      return () => {};
+    }, [])
+  );
 
   return (
     <View style={styles.card}>
       <Text style={styles.title}>Most Common Exercise</Text>
-      <Text style={styles.value}>{mostCommonExercise}</Text>
+      <Text style={styles.text}>{mostCommonExercise}</Text>
     </View>
   );
 }
@@ -204,29 +225,34 @@ function MostCommonExercise() {
 function AverageSetsAndReps() {
   const [averageData, setAverageData] = useState<AverageDataStructure>({});
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const config = await getAxiosConfig();
-        const response = await axios.get(
-          "http://192.168.1.106:5001/statistics/average-sets-reps",
-          config
-        );
-        console.log("Average Sets and Reps Response:", response.data);
-        setAverageData(response.data);
-      } catch (error) {
-        console.error("Axios Error:", error);
-      }
-    };
+  useFocusEffect(
+    React.useCallback(() => {
+      const fetchData = async () => {
+        try {
+          const config = await getAxiosConfig();
+          const response = await axios.get(
+            "http://192.168.1.106:5001/statistics/average-sets-reps",
+            config
+          );
+          console.log("Average Sets and Reps Response:", response.data);
+          setAverageData(response.data);
+        } catch (error) {
+          console.error("Axios Error:", error);
+        }
+      };
 
-    fetchData();
-  }, []);
+      fetchData();
+
+      // Cleanup function if needed
+      return () => {};
+    }, [])
+  );
 
   return (
     <View style={styles.card}>
-      <Text style={styles.title}>Average Sets and Reps</Text>
+      <Text style={styles.title}>Average Sets and Reps per Exercise</Text>
       {Object.keys(averageData).map((exercise) => (
-        <Text key={exercise}>
+        <Text key={exercise} style={styles.text}>
           {exercise}: {averageData[exercise].averageSets} sets,{" "}
           {averageData[exercise].averageReps} reps
         </Text>
@@ -238,28 +264,33 @@ function AverageSetsAndReps() {
 function WorkoutStreak() {
   const [streak, setStreak] = useState(0);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const config = await getAxiosConfig();
-        const response = await axios.get(
-          "http://192.168.1.106:5001/statistics/workout-streak",
-          config
-        );
-        console.log("Workout Streak Response:", response.data);
-        setStreak(response.data);
-      } catch (error) {
-        console.error("Axios Error:", error);
-      }
-    };
+  useFocusEffect(
+    React.useCallback(() => {
+      const fetchData = async () => {
+        try {
+          const config = await getAxiosConfig();
+          const response = await axios.get(
+            "http://192.168.1.106:5001/statistics/workout-streak",
+            config
+          );
+          console.log("Workout Streak Response:", response.data);
+          setStreak(response.data);
+        } catch (error) {
+          console.error("Axios Error:", error);
+        }
+      };
 
-    fetchData();
-  }, []);
+      fetchData();
+
+      // Cleanup function if needed
+      return () => {};
+    }, [])
+  );
 
   return (
     <View style={styles.card}>
       <Text style={styles.title}>Workout Streak</Text>
-      <Text>Current Streak: {streak} days</Text>
+      <Text style={styles.text}>Current Streak: {streak} days</Text>
     </View>
   );
 }
@@ -267,98 +298,124 @@ function WorkoutStreak() {
 function RecentCompletedWorkouts() {
   const [recentWorkouts, setRecentWorkouts] = useState<IWorkout[]>([]);
 
-  useEffect(() => {
-    const fetchRecentCompletedWorkouts = async () => {
-      try {
-        const config = await getAxiosConfig();
-        const response = await axios.get("http://192.168.1.106:5001/statistics/recent-completed-workouts", config);
-        setRecentWorkouts(response.data);
-      } catch (error) {
-        console.error("Axios Error:", error);
-      }
-    };
+  useFocusEffect(
+    React.useCallback(() => {
+      const fetchRecentCompletedWorkouts = async () => {
+        try {
+          const config = await getAxiosConfig();
+          const response = await axios.get("http://192.168.1.106:5001/statistics/recent-completed-workouts", config);
+          setRecentWorkouts(response.data);
+        } catch (error) {
+          console.error("Axios Error:", error);
+        }
+      };
 
-    fetchRecentCompletedWorkouts();
-  }, []);
+      fetchRecentCompletedWorkouts();
+
+      // Cleanup function if needed
+      return () => {};
+    }, [])
+  );
 
   return (
     <View style={styles.card}>
       <Text style={styles.title}>Recent Workouts</Text>
       {recentWorkouts.map(workout => (
-        <Text key={workout._id}>{workout.name}</Text>
+        <Text key={workout._id} style={styles.text}>{workout.name}</Text>
       ))}
     </View>
   );
 }
+
+
 function MostUsedExercise() {
   const [exercise, setExercise] = useState("");
 
-  useEffect(() => {
-    const fetchMostUsedExercise = async () => {
-      try {
-        const config = await getAxiosConfig();
-        const response = await axios.get("http://192.168.1.106:5001/statistics/most-used-exercise", config);
-        setExercise(response.data);
-      } catch (error) {
-        console.error("Axios Error:", error);
-      }
-    };
+  useFocusEffect(
+    React.useCallback(() => {
+      const fetchMostUsedExercise = async () => {
+        try {
+          const config = await getAxiosConfig();
+          const response = await axios.get("http://192.168.1.106:5001/statistics/most-used-exercise", config);
+          setExercise(response.data);
+        } catch (error) {
+          console.error("Axios Error:", error);
+        }
+      };
 
-    fetchMostUsedExercise();
-  }, []);
+      fetchMostUsedExercise();
+
+      // Cleanup function if needed
+      return () => {};
+    }, [])
+  );
 
   return (
     <View style={styles.card}>
       <Text style={styles.title}>Most Used Exercise</Text>
-      <Text style={styles.value}>{exercise}</Text>
+      <Text style={styles.text}>{exercise}</Text>
     </View>
   );
 }
+
+
 function TotalWeightLifted() {
   const [totalWeight, setTotalWeight] = useState(0);
 
-  useEffect(() => {
-    const fetchTotalWeightLifted = async () => {
-      try {
-        const config = await getAxiosConfig();
-        const response = await axios.get("http://192.168.1.106:5001/statistics/total-weight-lifted", config);
-        setTotalWeight(response.data);
-      } catch (error) {
-        console.error("Axios Error:", error);
-      }
-    };
+  useFocusEffect(
+    React.useCallback(() => {
+      const fetchTotalWeightLifted = async () => {
+        try {
+          const config = await getAxiosConfig();
+          const response = await axios.get("http://192.168.1.106:5001/statistics/total-weight-lifted", config);
+          setTotalWeight(response.data);
+        } catch (error) {
+          console.error("Axios Error:", error);
+        }
+      };
 
-    fetchTotalWeightLifted();
-  }, []);
+      fetchTotalWeightLifted();
+
+      // Cleanup function if needed
+      return () => {};
+    }, [])
+  );
 
   return (
     <View style={styles.card}>
       <Text style={styles.title}>Total Weight Lifted</Text>
-      <Text style={styles.value}>{totalWeight} kg/lbs</Text>
+      <Text style={styles.text}>{totalWeight} kg/lbs</Text>
     </View>
   );
 }
+
+
 function MostCommonMuscleGroup() {
   const [mostCommonGroup, setMostCommonGroup] = useState("");
 
-  useEffect(() => {
-    const fetchMostCommonMuscleGroup = async () => {
-      try {
-        const config = await getAxiosConfig();
-        const response = await axios.get("http://192.168.1.106:5001/statistics/most-common-muscle-group", config);
-        setMostCommonGroup(response.data);
-      } catch (error) {
-        console.error("Axios Error:", error);
-      }
-    };
+  useFocusEffect(
+    React.useCallback(() => {
+      const fetchMostCommonMuscleGroup = async () => {
+        try {
+          const config = await getAxiosConfig();
+          const response = await axios.get("http://192.168.1.106:5001/statistics/most-common-muscle-group", config);
+          setMostCommonGroup(response.data);
+        } catch (error) {
+          console.error("Axios Error:", error);
+        }
+      };
 
-    fetchMostCommonMuscleGroup();
-  }, []);
+      fetchMostCommonMuscleGroup();
+
+      // Cleanup function if needed
+      return () => {};
+    }, [])
+  );
 
   return (
     <View style={styles.card}>
       <Text style={styles.title}>Most Common Muscle Group</Text>
-      <Text style={styles.value}>{mostCommonGroup}</Text>
+      <Text style={styles.text}>{mostCommonGroup}</Text>
     </View>
   );
 }
@@ -367,6 +424,7 @@ function MostCommonMuscleGroup() {
 
 //sprobaj zadnji dve
 function WorkoutStatistics() {
+
   return (
     <ScrollView style={styles.container}>
       <CompletedWorkouts />
@@ -378,9 +436,7 @@ function WorkoutStatistics() {
       <WorkoutStreak />
       <MostUsedExercise />
       <MostCommonMuscleGroup />
-
       <LongestShortestWorkout /> 
-      <ExerciseTypeStats />
     </ScrollView>
   );
 }
@@ -388,8 +444,9 @@ function WorkoutStatistics() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#1a2d3d',
     padding: 16,
-    backgroundColor: '#1a2d3d'
+    paddingBottom: 32,  // Add this line
   },
   card: {
     padding: 20,
@@ -407,6 +464,12 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 24,
+    color: '#e5f4e3',
+    marginBottom: 8,
+    fontWeight: "bold"
+  },
+  text: {
+    fontSize: 18,
     color: '#e5f4e3',
     marginBottom: 8,
   },
